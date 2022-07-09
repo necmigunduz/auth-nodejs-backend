17b3526d8f3887dae6429f23e6388e4bb154ef08
necmigunduz <necmigunduz@gmail.com>
2022-07-09 10:58:11 +0300

    Add user model

---
+++ model/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, 'Please enter a username']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email adress']
+    },
+    phone: {
+        type: String,
+        required: [true, 'Please enter a phone number']
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
