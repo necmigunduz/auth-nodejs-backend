fbf534e53ec229a33484c7dda5cdd8d6f349f133
necmigunduz <necmigunduz@gmail.com>
2022-07-10 10:49:05 +0300

    Add cors

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const auth = require("./auth");
 // Connect to MongoDB
 const dbConnect = require("./db/dbConnect");
 dbConnect();
+// Handle Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 // body parser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
